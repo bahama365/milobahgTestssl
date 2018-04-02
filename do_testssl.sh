69caa6d8902cd61dfe7bf3f822b277b495ddcdb2
bahama365 <31001392+bahama365@users.noreply.github.com>
2018-04-02 12:55:23 +0100

    Update do_testssl.sh

--- do_testssl.sh
+++ do_testssl.sh
@@ -5,6 +5,13 @@
 # Script to run a TLS test against given hostname/IP(s) from a DigitalOcean 
 # droplet in London, then delete the droplet.
 #
+chatid="yourChatIDHere"
+token="yourTokenHere"
+
+telegramSend (){
+  curl -s -F chat_id=$chatid -F document=@$PWD/testSSL_$target.txt -F caption="TestSSL report for: $target" https://api.telegram.org/bot$token/sendDocument > /dev/null
+}
+
 clear
 echo "This program will create a DigitalOcean droplet based in the UK, run a TLS test and then delete itself."
   read -p "What is the hostname/IP of the target? (example.com, 194.54.23.43): " target
@@ -21,11 +28,12 @@ echo "30 Seconds left!..."
 echo "10 seconds left!..."
   sleep 10
 echo "Now running testSSL against the target: $target"...
-  doctl compute ssh testSSL --ssh-command "docker run --rm -t milobahg/testssl $target" | tee testSSL_$target
+  doctl compute ssh testSSL --ssh-command "docker run --rm -t milobahg/testssl $target" | tee testSSL_$target.txt
 echo
 echo "...done!"
   sleep 1
-echo "Output saved to: testSSL_$target"
+echo "Output saved to: testSSL_$target.txt"
+  telegramSend
   sleep 1
 echo "Now deleting the droplet permanently...."
   doctl compute droplet delete testSSL -f
